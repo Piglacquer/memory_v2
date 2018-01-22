c4cdbd6a258cc641546f1bbc0865974bdf02caf6
Patrick Biffle <pbiffle@gmail.com>
2018-01-22 09:44:08 -0700

    chanaged live server to production environment in knex file

--- knexfile.js
+++ knexfile.js
@@ -1,11 +1,12 @@
 module.exports = {
 	development: {
 		client: 'pg',
-		connection:
-			'postgres://aeikheoqbfnvau:4561f6806195fe38fa4e5f15332be97c859ec25b614922aceff034d714b9a268@ec2-54-235-252-137.compute-1.amazonaws.com:5432/ddssf6u3pcekma'
+		connection: 'postgres:///3000'
 	},
 	production: {
 		client: 'pg',
-		connection: process.env.DATABASE_URL
+		connection:
+			process.env.DATABASE_URL ||
+			'postgres://aeikheoqbfnvau:4561f6806195fe38fa4e5f15332be97c859ec25b614922aceff034d714b9a268@ec2-54-235-252-137.compute-1.amazonaws.com:5432/ddssf6u3pcekma'
 	}
 }
